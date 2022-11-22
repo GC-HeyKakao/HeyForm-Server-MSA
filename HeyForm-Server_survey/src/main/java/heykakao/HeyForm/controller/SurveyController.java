4427ab32003af94a0bfbdc57b98948efb8e3340a
parkchanbin54 <parks4754@gmail.com>
2022-11-22 11:39:25 +0900

    survey added

---
+++ HeyForm-Server_survey/src/main/java/heykakao/HeyForm/controller/SurveyController.java
@@ -0,0 +1,114 @@
+package heykakao.HeyForm.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import heykakao.HeyForm.exception.ResourceNotFoundException;
+import heykakao.HeyForm.exception.ResponseEntityHandler;
+
+import heykakao.HeyForm.model.dto.SurveyQuestionDto;
+import heykakao.HeyForm.repository.*;
+
+import heykakao.HeyForm.service.DtoService;
+import heykakao.HeyForm.service.SurveyService;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+@RestController
+//@RequestMapping("/api/v1/")
+@ResponseBody
+public class SurveyController {
+    @Autowired
+
+    private final DtoService dtoService;
+    private final SurveyService surveyService;
+
+
+    @Autowired
+    public SurveyController(DtoService dtoService, SurveyService surveyService){
+
+        this.dtoService = dtoService;
+        this.surveyService = surveyService;
+    }
+
+
+    //Surveyjson type 리턴값 : 설문 url
+    //{"surveyDto":{"survey_state":0,"survey_url":"c4ca4238a0b923820dcc509a6f75849b","start_time":1670727600000,"end_time":1670817600000,"category":null,"description":null},"questionDtos":[{"question_type":1,"question_order":2,"question_contents":"qs sample2 bla bla","choiceDtos":[{"choice_order":1,"choice_contents":"ch_sample1 bla bla bla"}]}]}
+    @PostMapping("/survey/{userToken}")
+    @ApiOperation(value = "설문조사 생성", notes = "사용자 token을 사용해서 설문조사를 생성한다. 생성된 설문조사의 페이지 Url이 리턴된다. (설문조사 제작) 양식 : {\"surveyDto\":{\"survey_state\":0,\"start_time\":\"2022-12-11 12:00\",\"end_time\":\"2022-12-11 13:00\",\"category\":null,\"description\":null},\"questionDtos\":[{\"question_type\":1,\"question_order\":2,\"question_contents\":\"qs sample2 bla bla\",\"choiceDtos\":[{\"choice_order\":1,\"choice_contents\":\"ch_sample1 bla bla bla\"}]}]}")
+    public String createSurvey(@RequestBody SurveyQuestionDto surveyDto, @PathVariable String userToken) throws Exception {
+//        if (!dtoService.tokencheck(userToken)){
+//            throw new ResourceNotFoundException(String.format("Expired token: %s",userToken));
+//        }
+        Long survey_id = dtoService.saveSurvey(userToken,surveyDto);
+        return surveyService.getUrl(survey_id);
+    }
+
+    //설문 정보를 url을 통해 전달한다.
+    @GetMapping("/survey/paper/{surveyUrl}")
+    @ApiOperation(value = "설문조사 페이지 생성", notes = "url을 이용해서 설문조사 정보를 불러온다. (설문조사 url)")
+    public SurveyQuestionDto createPaperByURL(@PathVariable String surveyUrl){
+        return dtoService.getSurveyQuestionByUrl(surveyUrl);
+    }
+
+
+
+
+    @DeleteMapping("/survey/{surveyId}")
+    @ApiOperation(value = "설문조사 제거", notes = "설문조사 id를 이용해서 설문조사를 제거한다. (마이페이지 / 설문조사 제거)")
+    public void deleteSurvey(@RequestParam Long surveyId, @RequestParam String userToken){
+        if (!dtoService.tokencheck(userToken)){
+            throw new ResourceNotFoundException(String.format("Expired token: %s",userToken));
+        }
+        surveyService.delSurvey(surveyId);
+    }
+
+    //surveyId를 통해 업데이트..
+    @PostMapping("/survey/update")
+    @ApiOperation(value = "설문조사 업데이트", notes = "설문조사를 업데이트한다. (마이페이지 / 설문조사 업데이트)")
+    public String updateSurvey(@RequestBody SurveyQuestionDto surveyQuestionDto, @RequestParam String userToken) throws JsonProcessingException, NoSuchAlgorithmException {
+        if (!dtoService.tokencheck(userToken)){
+            throw new ResourceNotFoundException(String.format("Expired token: %s",userToken));
+        }
+        return dtoService.updateSurvey(surveyQuestionDto);
+    }
+
+    //surveyId를 통해 설문지 정보 불러오기
+    @GetMapping("/survey/list/{surveyId}")
+    @ApiOperation(value = "설문조사 정보 조회" , notes = "설문조사 id를 이용해서 설문조사 정보를 불러온다. (마이페이지 / 설문조사 정보보기)")
+    public SurveyQuestionDto getSurveyInfoBySurveyId(@PathVariable Long surveyId, @RequestParam String userToken){
+        if (!dtoService.tokencheck(userToken)){
+            throw new ResourceNotFoundException(String.format("Expired token: %s",userToken));
+        }
+        SurveyQuestionDto surveyQuestionDto = dtoService.getSurveyQuestionBySurveyId(surveyId);
+        return surveyQuestionDto;
+    }
+
+    // userId를 통해 해당 유저의  survey, question, answer 정보 모두 불러오기
+    @GetMapping("/survey/total/{userToken}")
+    @ApiOperation(value = "사용자 정보 조회", notes = "사용자 token을 사용해서 사용자가 생성한 설문조사 리스트 가져오기. (마이페이지)")
+    public List<SurveyQuestionDto> getInfoByUserAccount(@PathVariable String userToken){
+//        if (!dtoService.tokencheck(userToken)){
+//            throw new ResourceNotFoundException(String.format("Expired token: %s",userToken));
+//        }
+        return dtoService.getSurveysByUserToken(userToken);
+    }
+
+    @GetMapping("/survey/check/{surveyUrl}")
+    @ApiOperation(value = "설문조사 url 조회", notes = "url이 존재하는지 확인")
+    public boolean checkUrl(@PathVariable String surveyUrl, @RequestParam String userToken){
+        if (!dtoService.tokencheck(userToken)){
+            throw new ResourceNotFoundException(String.format("Expired token: %s",userToken));
+        }
+        return dtoService.Urlcheck(surveyUrl);
+    }
+
+    // 테스트용
+
+}
---
+++ HeyForm-Server_survey/src/main/java/heykakao/HeyForm/service/DtoService.java
@@ -0,0 +1,371 @@
+package heykakao.HeyForm.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import com.google.gson.Gson;
+import heykakao.HeyForm.model.*;
+import heykakao.HeyForm.model.dto.*;
+import heykakao.HeyForm.repository.*;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import javax.swing.text.html.parser.Entity;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+
+@Service
+public class DtoService {
+    private final SurveyRepository surveyRepository;
+    private final QuestionRepository questionRepository;
+    private final ChoiceRepository choiceRepository;
+
+    private final QARepository qaRepository;
+    JWTService jwtService = new JWTService();
+
+    @Autowired
+    public DtoService(SurveyRepository surveyRepository, QuestionRepository questionRepository,
+                      ChoiceRepository choiceRepository, QARepository qaRepository) {
+        this.surveyRepository = surveyRepository;
+        this.questionRepository = questionRepository;
+        this.choiceRepository = choiceRepository;
+        this.qaRepository = qaRepository;
+    }
+
+    // Save
+//    public Long saveSurvey(String user_token, SurveyQuestionDto surveyQuestionDto) throws Exception {
+    public Long saveSurvey(String user_token, SurveyQuestionDto surveyQuestionDto) throws Exception {
+
+//        Object user_account = jwtService.getClaims(jwtService.getClaims(user_token,JWTService.SECRET_KEY),"email");
+//
+        SurveyDto surveyDto = surveyQuestionDto.getSurveyDto();
+
+        Long user_id = get_UserToken(user_token);
+
+
+
+        Survey survey = new Survey();
+        survey.setByDto(surveyDto, user_id);
+
+        surveyRepository.save(survey);
+        String url = makeUrl(survey.getId());
+        survey.setUrl(url);
+        surveyRepository.save(survey);
+        
+
+        List<QuestionDto> questionDtos = surveyQuestionDto.getQuestionDtos();
+
+        for (QuestionDto questionDto : questionDtos) {
+            Question question = new Question();
+            question.setByDto(questionDto, survey);
+            questionRepository.save(question);
+
+            List<ChoiceDto> choiceDtos = questionDto.getChoiceDtos();
+
+            for (ChoiceDto choiceDto : choiceDtos) {
+                Choice choice = new Choice();
+                choice.setByDto(choiceDto, question);
+                choiceRepository.save(choice);
+            }
+        }
+
+        return survey.getId();
+    }
+
+    public void saveQa(QADto qaDto){
+        QA qa = new QA();
+        qa.setByDto(qaDto);
+        qaRepository.save(qa);
+    }
+    public void saveQaAnswer(QADto qaDto){
+        QA qa = qaRepository.findById(qaDto.getQa_id()).get();
+        qa.setByDto(qaDto);
+        qaRepository.save(qa);
+    }
+    public void delQa(Long qa_id){
+        qaRepository.deleteById(qa_id);
+    }
+
+    //error x
+    private String makeUrl(Long survey_id) throws NoSuchAlgorithmException {
+        MessageDigest md = MessageDigest.getInstance("MD5");
+        byte[] messageDigest = md.digest(String.valueOf(survey_id).getBytes());
+        timecheck(survey_id);
+        BigInteger bigint = new BigInteger(1, messageDigest);
+        String hexText = bigint.toString(16);
+        while (hexText.length() < 32) {
+            hexText = "0".concat(hexText);
+        }
+
+        return hexText;
+    }
+
+
+
+
+    // Update
+    //error x
+    public void updateSurveyInfo(SurveyDto surveyDto) {
+
+        try {
+            Survey survey = surveyRepository.findById(surveyDto.getSurvey_id()).get();
+            survey.setByDto(surveyDto);
+            surveyRepository.save(survey);
+        } catch (Exception e) {
+            throw new IllegalStateException("해당 설문이 존재하지 않습니다");
+        }
+
+    }
+
+    public void updateQuestion(Long survey_id, QuestionDto questionDto) {
+        try {
+            surveyRepository.findById(survey_id);
+        } catch (Exception e) {
+            throw new IllegalStateException("해당 설문이 존재하지 않습니다.");
+        }
+        try {
+            Question question = questionRepository.findByOrderAndSurvey_Id(questionDto.getQuestion_order(), survey_id).get();
+            question.setByDto(questionDto);
+            questionRepository.save(question);
+        } catch (Exception e) {
+            throw new IllegalStateException("해당 질문이 존재하지 않습니다.");
+        }
+    }
+
+    public void updateChoice(Long question_id, ChoiceDto choiceDto) {
+        try {
+            Choice choice = choiceRepository.findByOrderAndQuestion_Id(choiceDto.getChoice_order(), question_id).get();
+            choice.setByDto(choiceDto);
+            choiceRepository.save(choice);
+        } catch (Exception e) {
+            throw new IllegalStateException("해당 choice가 존재하지 않습니다.");
+        }
+    }
+
+    //error x
+    public void updateAllChoices(Long question_id, List<ChoiceDto> choiceDtos) {
+        try {
+            questionRepository.findById(question_id);
+        } catch (Exception e) {
+            throw new IllegalStateException("해당 질문이 존재하지 않습니다.");
+        }
+        try {
+            List<Choice> choices = choiceRepository.findByQuestion_Id(question_id);
+            for (Choice choice : choices) {
+                ChoiceDto choiceDto = choiceDtos.stream().filter(ch_dto -> ch_dto.getChoice_order().equals(choice.getOrder())).collect(Collectors.toList()).get(0);
+                choice.setByDto(choiceDto);
+                choiceRepository.save(choice);
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException("해당 choice가 존재하지 않습니다.");
+        }
+    }
+
+    //error x
+    public void updateAllQuestions(Long survey_id, List<QuestionDto> questionDtos) {
+        try {
+            surveyRepository.findById(survey_id);
+        } catch (Exception e) {
+            throw new IllegalStateException("해당 설문이 존재하지 않습니다.");
+        }
+        try {
+            List<Question> questions = questionRepository.findBySurvey_Id(survey_id);
+            for (Question question : questions) {
+                QuestionDto questionDto = questionDtos.stream().filter(qs_dto -> qs_dto.getQuestion_order().equals(question.getOrder())).collect(Collectors.toList()).get(0);
+                question.setByDto(questionDto);
+                questionRepository.save(question);
+                updateAllChoices(question.getId(), questionDto.getChoiceDtos());
+            }
+        } catch (Exception e) {
+            throw new IllegalStateException("해당 질문이 존재하지 않습니다,");
+        }
+    }
+
+    //error x
+    public String updateSurvey(SurveyQuestionDto surveyQuestionDto) throws NoSuchAlgorithmException {
+
+        SurveyDto surveyDto = surveyQuestionDto.getSurveyDto();
+        List<QuestionDto> questionDtos = surveyQuestionDto.getQuestionDtos();
+        updateSurveyInfo(surveyDto);
+        updateAllQuestions(surveyDto.getSurvey_id(), questionDtos);
+        Survey survey = surveyRepository.getById(surveyDto.getSurvey_id());
+        survey.setUrl(makeUrl(surveyDto.getSurvey_id()));
+        surveyRepository.save(survey);
+        return survey.getUrl();
+    }
+
+
+
+    // Get
+    public List<SurveyQuestionDto> getSurveysByUserToken(String user_token) {
+        try {
+//            Object user_account = jwtService.getClaims(jwtService.getClaims(user_token,JWTService.SECRET_KEY),"email");
+            Long user_id = get_UserToken(user_token);
+            return getSurveyQuestionDtos(user_id);
+        } catch (Exception e) {
+            throw new IllegalStateException("일치 정보가 없습니다.");
+        }
+    }
+
+
+    public SurveyQuestionDto getSurveyQuestionBySurveyId(Long survey_id) {
+
+        try {
+            return getSurveyQuestionDto(survey_id);
+        } catch (Exception e) {
+            throw new RuntimeException("없는 설문조사입니다.");
+        }
+    }
+
+    public SurveyQuestionDto getSurveyQuestionByUrl(String survey_url) {
+        try {
+            Survey survey = surveyRepository.findByUrl(survey_url).get();
+            return survey2surveyQuestionDto(survey);
+        } catch (Exception e) {
+            throw new IllegalStateException("해당 설문이 존재하지 않습니다.");
+        }
+    }
+
+
+
+    private SurveyQuestionDto survey2surveyQuestionDto(Survey survey) {
+        try {
+            SurveyDto surveyDto = new SurveyDto(survey);
+            List<QuestionDto> questionDtos = new ArrayList<>();
+            List<Question> questions = questionRepository.findBySurvey_Id(survey.getId());
+
+            for (Question question : questions) {
+                questionDtos.add(question2questionDto(question));
+            }
+
+            return new SurveyQuestionDto(surveyDto, questionDtos);
+        } catch (Exception e) {
+            throw new IllegalStateException("오류");
+        }
+    }
+
+    private QuestionDto question2questionDto(Question question) {
+        try {
+            List<Choice> choices = choiceRepository.findByQuestion_Id(question.getId());
+            List<ChoiceDto> choiceDtos = new ArrayList<>();
+            for (Choice choice : choices) {
+                ChoiceDto choiceDto = new ChoiceDto(choice);
+                choiceDtos.add(choiceDto);
+            }
+            return new QuestionDto(question, choiceDtos);
+        } catch (Exception e) {
+            throw new IllegalStateException("오류");
+        }
+    }
+
+    private List<ChoiceDto> getChoiceDtos(Long question_id) {
+        List<ChoiceDto> choiceDtos = new ArrayList<>();
+        List<Choice> choices = choiceRepository.findByQuestion_Id(question_id);
+        for (Choice choice : choices) {
+            ChoiceDto choiceDto = new ChoiceDto(choice);
+            choiceDtos.add(choiceDto);
+        }
+        return choiceDtos;
+    }
+
+    private List<QuestionDto> getQuestionDtos(Long survey_id) {
+        List<Question> questions = questionRepository.findBySurvey_Id(survey_id);
+        List<QuestionDto> questionDtos = new ArrayList<>();
+        for (Question question : questions) {
+            List<ChoiceDto> choiceDtos = getChoiceDtos(question.getId());
+
+            QuestionDto questionDto = new QuestionDto(question, choiceDtos);
+            questionDtos.add(questionDto);
+        }
+        return questionDtos;
+    }
+
+    //여기
+    private SurveyQuestionDto getSurveyQuestionDto(Long survey_id) {
+        Survey survey = surveyRepository.findById(survey_id).get();
+        SurveyDto surveyDto = new SurveyDto(survey);
+        timecheck(surveyDto.getSurvey_id());
+        List<QuestionDto> questionDtos = getQuestionDtos(survey_id);
+
+        return new SurveyQuestionDto(surveyDto, questionDtos);
+    }
+
+    //여기
+    private List<SurveyQuestionDto> getSurveyQuestionDtos(Long user_id) {
+        List<SurveyQuestionDto> surveyQuestionDtos = new ArrayList<>();
+        List<Long> survey_ids = surveyRepository.findByUser_id(user_id).stream().map(Survey::getId).collect(Collectors.toList());
+        for (Long survey_id : survey_ids) {
+            timecheck(survey_id);
+            surveyQuestionDtos.add(getSurveyQuestionDto(survey_id));
+        }
+        return surveyQuestionDtos;
+    }
+
+
+
+    private void timecheck(Long survey_id){
+        Survey survey = surveyRepository.getById(survey_id);
+        Timestamp start_time = Timestamp.valueOf(survey.getStarttime());
+        Timestamp end_time = Timestamp.valueOf(survey.getEndtime());
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+        if(end_time.before(now)){
+            survey.setState(2);
+        }
+        else if (start_time.after(now)){
+            survey.setState(0);
+        }
+    }
+    public boolean Urlcheck(String url){
+        if (surveyRepository.findByUrl(url).isPresent()){return true;}
+        else{return false;}
+    }
+
+
+
+
+    public boolean tokencheck(String token){
+
+        if (!jwtService.validateToken(token)){
+            return false;
+        }
+        else{
+            return true;
+        }
+    }
+
+
+
+    public Long get_UserToken(String target) throws Exception {
+        String result = "";
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(target);
+
+        try{
+            HttpClient client = new DefaultHttpClient();
+            MyHttpGetWithEntity e = new MyHttpGetWithEntity("http://localhost:8080/user/token/service/"+target);
+            HttpResponse response = client.execute(e);
+            System.out.println(response.getStatusLine().getStatusCode());
+            if (response.getStatusLine().getStatusCode() == 200) {
+                ResponseHandler<String> handler = new BasicResponseHandler();
+                result = handler.handleResponse(response);
+            }
+            System.out.println(result);
+
+        }catch (Exception e){System.err.println(e);}
+
+        return Long.parseLong(result);
+    }
+
+}
